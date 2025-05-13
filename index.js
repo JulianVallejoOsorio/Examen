83a8c3219d09e18f266a10af1c3dc1e46e756640
julianvallejoosorio <julianvallejoosorio@gmail.com>
2025-05-13 14:33:25 -0500

    commit

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-// Ejercicio 4. Cálculo de la prima de servicios
-// Contexto: En una empresa, se necesita calcular la prima de servicios para cada
-// empleado. Esta prima se calcula en función del tiempo trabajado y el salario
-// mensual, pero también depende de la cantidad de ausencias injustificadas del
-// empleado. Si un empleado ha tenido más de 5 ausencias injustificadas, se le debe
-// deducir un porcentaje de la prima. Además, si el empleado no ha cumplido 6 meses
-// en la empresa, la prima debe calcularse proporcionalmente a los días trabajados.
-// Condiciones:
-// 1. Se debe calcular la prima con base en el salario mensual y el tiempo
-// trabajado.
-// 2. Si el empleado ha tenido más de 5 ausencias injustificadas, se le deduce un
-// porcentaje de la prima.
-// 3. Si el empleado ha trabajado menos de 6 meses, se calcula una prima
-// proporcional.
-// 4. El algoritmo debe calcular la prima de forma precisa para cada empleado
-// según las condiciones dadas.
-// Instrucciones para resolver con arreglos:
-// • Crea un arreglo de objetos empleados, donde cada objeto tenga: nombre,
-// salario, mesesTrabajados, ausencias.
-// CESDE
-// Programa Técnico laboral como asistente en desarrollo de Software
-// Módulo Programación Web 1
-// Sede Medellín
-// Docente Jaime Zapata Valencia
-// Denominación Desempeño
-// • Recorre el arreglo para calcular la prima considerando proporcionalidad y
-// descuentos.
-// • Almacena los resultados en un nuevo arreglo de resultados.
-const empleados = [];
 
+const empleados = [];
 function menu() {
     let opcion;
     do {
